712a6d116b9a1743c41242dd341e0866ea9cfed2
wangzhen <1050586287@qq.com>
2019-02-22 18:19:13 +0800

    给工程添加一个文件

---
+++ src/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+
+    }
+
+}
